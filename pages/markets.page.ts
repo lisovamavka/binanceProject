3874aa2e1d1efcafd180a329068e378c490e174f
jkep-ved <v8288287@gmail.com>
2026-03-30 20:50:53 +0300

    modifie file

--- pages/markets.page.ts
+++ pages/markets.page.ts
@@ -12,10 +12,10 @@ export class MarketsPage extends BasePage {
 
     async goto(): Promise<void> {
         await this.page.goto('https://www.binance.com/en/markets/alpha-all', {
+            // Using 'domcontentloaded' to make loading faster instead of default 'load' event, which waits for all resources. This is sufficient for our tests that focus on DOM elements.
             waitUntil: 'domcontentloaded',
         });
-
+        
         await this.dismissBlockingOverlays();
-        await this.topTabs.waitUntilLoaded();
     }
 }
