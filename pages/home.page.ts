97984564144fdb78171e5a29b53768d94b3c109a
DeadShank <rigenkih1996@gmail.com>
2026-03-31 14:41:45 +0300

    C56 - switch theme

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,16 +1,18 @@
-import { Page, Locator } from "@playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
 import { RootHeader } from "./components/root-header.component";
 import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
     public header: RootHeader;
     readonly appleLoginButton: Locator;
+    readonly themeToggleButton: Locator;
     private overlayHandlersRegistered = false;
 
     constructor(page: Page) {
         super(page);
         this.header = new RootHeader(page);
         this.appleLoginButton = page.locator('#apple-login > .third-part-btn');
+        this.themeToggleButton = page.locator('.bn-svg.theme-icon');
     }
 
     async goto() {
@@ -20,5 +22,11 @@ export class HomePage extends BasePage {
 
     async getTitle() {
         return await this.page.title();
+    }
+
+    async verifyButtonAndSwitchTheme() {
+        await expect(this.themeToggleButton).toBeVisible();
+        await expect(this.themeToggleButton).toBeEnabled();
+        await this.themeToggleButton.click();
     }
 }
---
+++ tests/switch-theme.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/home.page';
+
+test.describe.only('Switch Theme', () => {
+  test('Go to the home page and switch theme', async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    await test.step('Go to the home page', async () => {
+      await homePage.goto();
+    });
+
+    const beforeThemeChangedTime = await page.evaluate(() =>
+      localStorage.getItem('themeChangedTime'),
+    );
+
+    await test.step('Switch theme', async () => {
+      await homePage.verifyButtonAndSwitchTheme();
+    });
+
+    await expect
+      .poll(async () => page.evaluate(() => localStorage.getItem('themeChangedTime')))
+      .not.toBe(beforeThemeChangedTime);
+  });
+});
