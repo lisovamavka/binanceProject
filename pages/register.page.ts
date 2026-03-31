dc01affaf506fce7e09289ea9b5e0b6145d3d0b7
Vitalii Yanchuk <>
2026-03-31 21:02:00 +0300

    KAN17-18 added email validation tests

--- pages/register.page.ts
+++ pages/register.page.ts
@@ -4,15 +4,31 @@ import { BasePage } from "./base.page";
 
 export class RegisterPage extends BasePage {
     public header: RootHeader;
+
+    // username input
+    readonly usernameInput: Locator;
+    // username hints
+    readonly usernameHints: Locator;
+    // error messages
+    readonly usernameError: Locator;
+    readonly emailError: Locator;
+    // buttons
+    readonly continueButton: Locator;
     readonly continueWithGoogleButton: Locator;
-    readonly googleLoginIframe: Locator;
     readonly continueWithAppleButton: Locator;
     readonly continueWithTelegramButton: Locator;
+    // iframe
+    readonly googleLoginIframe: Locator;
 
 
     constructor(page: Page) {
         super(page);
         this.header = new RootHeader(page);
+        this.usernameInput = page.getByRole('textbox', { name: 'username' });
+        this.usernameHints = page.getByRole('listbox', { name: 'Email domain suggestions' });
+        this.usernameError = page.getByText('Please enter a valid email or phone number');
+        this.emailError = page.getByText('Invalid email address');
+        this.continueButton = page.getByRole('button', { name: 'Continue', exact: true });
         this.continueWithAppleButton = page.getByRole('button', { name: 'Continue with Apple' });
         this.continueWithGoogleButton = page.locator('button[aria-label="Continue with Google"]');
         this.continueWithTelegramButton = page.getByRole('button', { name: 'Continue with Telegram' });
---
+++ tests/invalid-email-validation.spec.ts
@@ -0,0 +1,61 @@
+import test, { expect} from "@playwright/test";
+import { RegisterPage } from "../pages/register.page";
+
+const invalidCredentials = [
+    '   ',
+    '',
+    '1',
+    '12',
+    '123',
+    '1234',
+    '12345',
+    '09635t4333',
+    'test@test',
+    'test@tes',
+    'test@123'
+];
+
+const validCredentials = [
+    '+380963524333',
+    '0963524333',
+    'y4uk.v@gmail.com',
+    'y4uk.v@gmail.co',
+];
+
+function isPotentiallyEmail(email: string): boolean {
+    return email.includes('@');
+}
+
+const expectErrorMessage = (page: RegisterPage, credential: string) => {
+    return isPotentiallyEmail(credential) ? expect(page.emailError) : expect(page.usernameError);
+}
+
+test.describe('C57,C58 - Email validation - Invalid credentials', () => {
+    for (const credential of invalidCredentials) {
+        test(`Invalid credential: ${credential}`, async ({ page}) => {
+            const registerPage = new RegisterPage(page);
+            await registerPage.goto();
+            await registerPage.usernameInput.fill(credential);
+            await registerPage.usernameInput.blur();
+            await registerPage.page.locator('body').click({ position: { x: 0, y: 0 } });
+            await expect(registerPage.usernameHints).toBeHidden();
+            await registerPage.continueButton.click();
+            await expectErrorMessage(registerPage, credential).toBeVisible();
+        });
+    }
+});
+
+test.describe('C57,C58 - Email validation - Valid credentials', () => {
+    for (const credential of validCredentials) {
+        test(`Valid credential: ${credential}`, async ({ page }) => {
+            const registerPage = new RegisterPage(page);
+            await registerPage.goto();
+            await registerPage.usernameInput.fill(credential);
+            await registerPage.usernameInput.blur();
+            await registerPage.page.locator('body').click({ position: { x: 0, y: 0 } });
+            await expect(registerPage.usernameHints).toBeHidden();
+            await registerPage.continueButton.click();
+            await expectErrorMessage(registerPage, credential).toBeHidden();
+        });
+    }
+});
